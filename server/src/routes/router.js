6b40038d13e807232a6808f19110e84d91e4ab6b
flaviare1s <flaviarster@gmail.com>
2025-08-16 09:58:20 -0300

    feat: login router

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,19 +1,49 @@
+import bcrypt from "bcrypt";
 import express from "express";
-import appointmentController from "../controllers/AppointmentController.js";
-import doctorController from "../controllers/DoctorController.js";
-import patientController from "../controllers/PatientController.js";
-import prescriptionController from "../controllers/PrescriptionController.js";
+import verifyToken from "../middleware/authMiddleware.js";
+import doctorService from "../services/DoctorService.js";
+
+import AppointmentController from "../controllers/AppointmentController.js";
+import DoctorController from "../controllers/DoctorController.js";
+import PacientController from "../controllers/PatientController.js";
+import PrescriptionController from "../controllers/PrescriptionController.js";
+
+import jwt from "jsonwebtoken";
 
 let router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("MEDAPP");
-  res.status(200).json({ message: "MEDAPP" });
+router.get("/", function (req, res) {
+  console.log("hi!");
+  res.status(200).json({ message: "hi!" });
 });
 
-router.use("/", appointmentController);
-router.use("/", doctorController);
-router.use("/", patientController);
-router.use("/", prescriptionController);
+// Mapeamento do login
+router.post("/login", async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    const doctor = await doctorService.getDoctorByLogin(login);
+    if (!doctor) {
+      return res.status(401).json({ error: "Authentication failed!" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, doctor.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed!" });
+    }
+
+    const token = jwt.sign({ doctorId: doctor._id }, "you-secret-key", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Login failed!" });
+  }
+});
+
+router.use("/", verifyToken, AppointmentController);
+router.use("/", verifyToken, DoctorController);
+router.use("/", verifyToken, PacientController);
+router.use("/", verifyToken, PrescriptionController);
 
 export default router;
